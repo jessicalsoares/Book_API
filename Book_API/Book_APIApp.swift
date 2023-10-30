0bf0047ceb25dfff41632e3f568f73df34c1b4dd
Jessica Soares <jess.soares@hotmail.com>
2023-10-30 10:50:33 +0000

    Initial Commit

---
+++ Book_API/Book_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Book_APIApp.swift
+//  Book_API
+//
+//  Created by Jessica Soares on 30/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Book_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
